04402a33200e34764efc9555805526a1190559d5
evacstz <evalohane10@gmail.com>
2024-11-17 16:55:54 -0300

    pasta para testes

---
+++ testes/teste.js
@@ -0,0 +1,61 @@
+// const cor = "rosa";
+
+// switch (cor) {
+//   case "vermelho":
+//     console.log("A cor é vermelho.");
+//    break;
+//   case "azul":
+//    console.log("A cor é azul.");
+//      break;
+// //   case "verde":
+// //     console.log("A cor é verde.");
+// //     break;
+// //   case "rosa":
+// //     console.log("A cor é rosa.");
+// //     break;
+// //   default:
+// //     console.log("Cor desconhecida.");
+// // }
+
+// const dia = 3;
+
+// switch (dia) {
+//   case 1:
+//     console.log("Segunda-feira");
+//   case 2:
+//     console.log("Terça-feira");
+//   case 3:
+//     console.log("Quarta-feira");
+//   case 4:
+//     console.log("Quinta-feira");
+//   default:
+//     console.log("Fim de semana");
+// }
+
+// const animal = "Jaguatirica";
+
+// switch (animal) {
+//   case "cachorro":
+//   case "gato":
+//     console.log("Este é um animal doméstico.");
+//     break;
+//   case "leão":
+//   case "tigre":
+//     console.log("Este é um animal selvagem.");
+//     break;
+//   default:
+//     console.log("Animal desconhecido.");
+// }
+
+
+// const numero = 9 * 3;
+// switch (numero) {
+//   case 5:
+//     console.log("É 5");
+//     break;
+//   case 6:
+//     console.log("É 6");
+//     break;
+//   default:
+//     console.log('Outro resultado');
+// }
